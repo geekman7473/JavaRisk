c723b742cf55985d2be7835b9e6c55e716da6adc
Ayernam <namhaza4@outlook.com>
2014-06-12 10:48:15 -0400

    Adding territories
    One by one by one

--- src/Graphics.java
+++ src/Graphics.java
@@ -40,14 +40,13 @@ public class Graphics {
 		//board.add(button);
 		
 		//Territories
-		/*
 		Continent continent[] = {new Continent("North America", 5), new Continent("South America", 2), new Continent("Europe", 5), new Continent("Africa", 3), new Continent("Asia", 7),  new Continent("Australia", 2)};
-		Territory territory[] = {new Territory(60, 76, "Alaska", continent[0], ), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory()};
+		Territory territory[] = {new Territory(60, 76, "Alaska", continent[0], 0), new Territory(108, 112, "Alberta", continent[0], 1), new Territory(120, 234, "Central America", continent[0], 2), new Territory(170, 180, "Eastern United States", continent[0], 3), new Territory(252, 47, "Greenland", continent[0], 4), new Territory(108, 47, "Northwest Terriory", continent[0], 5), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory()};
 		for(Territory i: territory){
 			board.add(i.getButton());
 		} 
 		
-
+		/*
 		//Button ActionListener
 		button.addActionListener(new ActionListener()
 		{
