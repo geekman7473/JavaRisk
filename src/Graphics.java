012a76f4fbae37a9b632122ad71be4dacd3ac2b4
Ayernam <namhaza4@outlook.com>
2014-06-14 12:13:41 -0400

    All territories and button listeners added

--- src/Graphics.java
+++ src/Graphics.java
@@ -41,20 +41,19 @@ public class Graphics {
 		
 		//Territories
 		Continent continent[] = {new Continent("North America", 5), new Continent("South America", 2), new Continent("Europe", 5), new Continent("Africa", 3), new Continent("Asia", 7),  new Continent("Australia", 2)};
-		Territory territory[] = {new Territory(55, 76, "Alaska", continent[0], 0), new Territory(108, 112, "Alberta", continent[0], 1), new Territory(120, 234, "Central America", continent[0], 2), new Territory(170, 180, "Eastern United States", continent[0], 3), new Territory(252, 47, "Greenland", continent[0], 4), new Territory(111, 76, "Northwest Terriory", continent[0], 5), new Territory(162, 128, "Ontario", continent[0], 6), new Territory(210, 121, "Quebec", continent[0], 7), new Territory(111, 165, "Western United States", continent[0], 8), new Territory(188, 381, "Argentina", continent[1], 9), new Territory(223, 316, "Brazil", continent[1], 10), new Territory(181, 339, "Peru", continent[1], 11), new Territory(167, 272, "Venezuela", continent[1], 12), new Territory(297, 153, "Great Britain", continent[2], 13), new Territory(312, 99, "Iceland", continent[2], 14), new Territory(367, 160, "Northern Europe", continent[2], 15), new Territory(377, 80, "Scandanavia", continent[2], 16), new Territory(382, 208, "Sourthern Europe", continent[2], 17), new Territory(434, 117, "Ukraine", continent[2], 18), new Territory(320, 210, "Western Europe", continent[2], 19), new Territory(399, 363, "Congo", continent[3], 20), new Territory(423, 326, "East Africa", continent[3], 21), new Territory(390, 277, "Egypt", continent[3], 22), new Territory(470, 429, "Madagascar", continent[3], 23), new Territory(340, 300, "North Africa", continent[3], 24), new Territory(400, 422, "South Africa", continent[3], 25), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory(), new Territory()};
-		for(Territory i: territory){
+		Territory territory[] = {new Territory(55, 76, "Alaska", continent[0], 0), new Territory(108, 112, "Alberta", continent[0], 1), new Territory(120, 234, "Central America", continent[0], 2), new Territory(170, 180, "Eastern United States", continent[0], 3), new Territory(252, 47, "Greenland", continent[0], 4), new Territory(111, 76, "Northwest Terriory", continent[0], 5), new Territory(162, 128, "Ontario", continent[0], 6), new Territory(210, 121, "Quebec", continent[0], 7), new Territory(111, 165, "Western United States", continent[0], 8), new Territory(188, 381, "Argentina", continent[1], 9), new Territory(223, 316, "Brazil", continent[1], 10), new Territory(181, 339, "Peru", continent[1], 11), new Territory(167, 272, "Venezuela", continent[1], 12), new Territory(297, 153, "Great Britain", continent[2], 13), new Territory(312, 99, "Iceland", continent[2], 14), new Territory(367, 160, "Northern Europe", continent[2], 15), new Territory(377, 80, "Scandanavia", continent[2], 16), new Territory(382, 208, "Sourthern Europe", continent[2], 17), new Territory(434, 117, "Ukraine", continent[2], 18), new Territory(320, 210, "Western Europe", continent[2], 19), new Territory(399, 363, "Congo", continent[3], 20), new Territory(423, 326, "East Africa", continent[3], 21), new Territory(390, 277, "Egypt", continent[3], 22), new Territory(470, 429, "Madagascar", continent[3], 23), new Territory(340, 300, "North Africa", continent[3], 24), new Territory(400, 422, "South Africa", continent[3], 25), new Territory(496, 177, "Afghanistan", continent[4], 26), new Territory(581, 215, "China", continent[4], 27), new Territory(533, 248, "India", continent[4], 28), new Territory(592, 123, "Irkutsk", continent[4], 29), new Territory(675, 177, "Japan", continent[4], 30), new Territory(655, 63, "Kamchatka", continent[4], 31), new Territory(448, 243, "Middle East", continent[4], 32), new Territory(596, 169, "Mongolia", continent[4], 33), new Territory(592, 275, "Siam", continent[4], 34), new Territory(544, 83, "Siberia", continent[4], 35), new Territory(503, 108, "Ural", continent[4], 36), new Territory(600, 64, "Yakutsk", continent[4], 37), new Territory(703, 424, "Eastern Australia", continent[5], 38), new Territory(611, 356, "Indonesia", continent[5], 39), new Territory(672, 339, "New Guinea", continent[5], 40), new Territory(636, 424, "Western Australia", continent[5], 41)};
+		for(final Territory i: territory){
+			//Add button
 			board.add(i.getButton());
-		} 
-		
-		/*
-		//Button ActionListener
-		button.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent arg0) {
-				System.out.println("meow");
-			}
-		});
-		*/
+			
+			//Button ActionListener
+			i.getButton().addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent arg0) {
+					System.out.println(i.getName());
+				}
+			});
+		}		
 		
 		//Frame settings
 		frame.setLayout(layout);
